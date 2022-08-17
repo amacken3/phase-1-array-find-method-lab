5b8da34025fcb06de8ffb004d9deeba2b0400b26
Aengus <aengus.2000@gmail.com>
2022-08-17 15:32:32 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin (record) {
+    let found = record.find(element => element.result === "W") 
+    if (found === undefined) {
+        return undefined
+    }
+    else if (found.result === 'W') { 
+    return found.year}
+}  
